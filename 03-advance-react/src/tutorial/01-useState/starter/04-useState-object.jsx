fb5c08b540a49184e976ae7bbdad1c8ad5b40ac5
chpraveen2905 <chpraveen2905@gmail.com>
2023-11-21 16:29:54 -0500

    Advance React

--- 03-advance-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advance-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,32 @@
 import { useState } from 'react';
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [person, setPerson] = useState({
+    name: 'Spider',
+    age: 28,
+    occupation: 'developer',
+  });
+  const display = () => {
+    setPerson({
+      name: 'Johny',
+      age: 30,
+      occupation: 'designer',
+    });
+
+    // be careful, don't overwrite
+    // setPerson({ name: 'susan' });
+    // setPerson({ ...person, name: 'susan' });
+  };
+  return (
+    <div>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.occupation}</h3>
+      <button type='button' className='btn' onClick={display}>
+        Show John
+      </button>
+    </div>
+  );
 };
 
 export default UseStateObject;
